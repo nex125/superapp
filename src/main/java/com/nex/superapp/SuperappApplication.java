32477734ebe2fae76b274897f2c3c828401fea98
Ilya Korzhenko <ilya.karzhenka@gmail.com>
2024-01-23 15:29:17 +0300

    Initial commit

---
+++ src/main/java/com/nex/superapp/SuperappApplication.java
@@ -0,0 +1,13 @@
+package com.nex.superapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuperappApplication.class, args);
+    }
+
+}
